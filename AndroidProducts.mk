ae56d34dd758936557d91d51aa43ca3367e94d0e
纳瑞泽😼 <raynv9@gmail.com>
2026-04-06 19:32:21 +0700

    Update copyright and product makefile references

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,12 @@
 #
-# Copyright (C) 2025 The LineageOS Project
+# Copyright (C) 2025 Project Matrixx
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tanzanite.mk
+    $(LOCAL_DIR)/matrixx_tanzanite.mk
+
+COMMON_LUNCH_CHOICES := \
+    matrixx_tanzanite-bp4a-user \
+    matrixx_tanzanite-bp4a-userdebug
