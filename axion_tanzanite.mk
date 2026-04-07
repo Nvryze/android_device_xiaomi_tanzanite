9a4fe28336733febb0f3cbbada932f8f0b19b168
纳瑞泽😼 <raynv9@gmail.com>
2026-04-07 18:09:53 +0700

    Add axion_tanzanite.mk for device configuration

---
+++ axion_tanzanite.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Axion stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from tanzanite device
+$(call inherit-product, device/xiaomi/tanzanite/device.mk)
+
+PRODUCT_DEVICE := tanzanite
+PRODUCT_NAME := axion_tanzanite
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 24117RN76O
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_SYSTEM_NAME := tanzanite_n_gl
+PRODUCT_SYSTEM_DEVICE := tanzanite
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=Redmi/tanzanite_n_global/tanzanite:15/AP3A.240905.015.A2/OS2.0.213.0.VOGEUXM:user/release-keys \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+# Axion flags
+TARGET_DISABLE_EPPE := true
+TARGET_INCLUDE_VIPERFX := true
+TARGET_ENABLE_BLUR := true
+
+AXION_MAINTAINER := Nvryze
+AXION_PROCESSOR := MediaTek_Helio_G99_Ultra
+AXION_CAMERA_REAR_INFO := 108,8,2
+AXION_CAMERA_FRONT_INFO := 16
